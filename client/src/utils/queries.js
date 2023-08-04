8d4bd637c288f8df5f8bee5a05198a07058fef18
Jose <josejrod07@gmail.com>
2023-08-04 19:47:05 -0400

    implemented useQuery and useMutation hooks

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 // query to retrieve logged in user's information (needs to be authenticated in order to run this query)
-export const QUERY_ME = gql`
+export const GET_ME = gql`
     {
         me {
             _id
